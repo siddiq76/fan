0dab08e38a10fe67ad4a7659bdecec65770ac7e1
“siddiq76” <siddiq.khan0786@gmail.com>
2021-02-02 13:26:27 +0530

    added some changes

---
+++ fanUC1.java
@@ -0,0 +1,18 @@
+public class fanUC1
+
+	public static void main(String args([])
+{
+	// create the point2D object
+	point2D point 2d_1 = new point2D(20.of, 150.of);
+	double x,y;
+	
+	// get the coordinates of the point
+	x = point2d_1.getX() ;
+	y = point2d_1.gety() ;
+	// display
+	System.out.println("X coordinate = " + X + " , y coordinates = " + y);
+	
+	// print distance
+	System.out.println("distance from origin = " + point2d_1.distance(0, 0));
+	}
+}
